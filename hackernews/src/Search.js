626641085f6a6f1d0bbc52078bdf6052ba89bb8a
Cody Thompson <cody.thompson22@gmail.com>
2018-02-04 18:09:38 -0800

    focus search field on did mount

--- hackernews/src/Search.js
+++ hackernews/src/Search.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Search = ({
-  value,
-  onChange,
-  onSubmit,
-  children
-}) =>
-  <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
-  </form>
+class Search extends Component {
+  constructor () {
+    super();
+    this.canSelect = this.canSelect.bind(this);
+    this.select = this.select.bind(this);
+  }
 
+  componentDidMount () {
+    if (this.input) {
+      this.input.focus();
+    }
+  }
+  
+  canSelect () {
+    return this.input && typeof this.input.select === 'function';
+  }
+
+  select () {
+    this.canSelect() && this.input.select();
+  }
+
+  render () {
+    const {
+      value,
+      onChange,
+      onSubmit,
+      children
+    } = this.props;
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          onFocus={this.select}
+          ref={(node) => {this.input = node; }}
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    );
+  }
+};
 export default Search;
